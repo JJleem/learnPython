5c6bfa224552a65990ee2e0a68ecd0658db8a558
JJleem <leemjaejun@gmail.com>
2025-05-28 12:24:40 +0900

    Started 2 days ago

---
+++ 2025_05_28.py
@@ -0,0 +1,40 @@
+# a = 10
+
+# if a == 5:
+#     print("correct")
+# e
+
+
+# password_correct =False
+
+# if password_correct:
+#     print("Welcome to the system")
+# elif password_correct == False:
+#     print("Welcome to the system, you are logged in")
+# else:
+#     print("Wrong password, try again")
+
+
+# winner = 10
+
+# if winner > 10:
+#     print("winner is greater than 10")
+# elif winner < 10:
+#     print("winner is less than 20")
+# else:
+#     print("You are the third winner")
+
+
+print("Hello, World!")
+
+age = int(input("How old are you?"))
+
+print(f"You are {age} years old.")
+
+type(age)
+print(type(age))
+
+if age < 18:
+    print("You are a minor.")
+elif age < 65:
+    print("You are an adult.")
