9f867ca14799cfc43af6a8c2975663453ccdce49
JJleem <leemjaejun@gmail.com>
2025-06-26 12:26:27 +0900

    Started 22 days ago

--- learnDjango/myproject/myapp/urls.py
+++ learnDjango/myproject/myapp/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'myapp'
+
 urlpatterns = [
-    path('', views.home, name='home'),]
+    path('', views.home, name='home'),
+    path('detail/<int:question_id>/', views.detail, name='detail'),
+    path('results/<int:question_id>/', views.results, name='results'),
+    path('vote/<int:question_id>/', views.vote, name='vote'),
+    ]
+
--- learnDjango/myproject/myapp/views.py
+++ learnDjango/myproject/myapp/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from django.template import loader
+from django.http import Http404
+from .models import Question, Choice
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello Djangod!")    
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('myapp/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))     
+    
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'myapp/detail.html', {'question': question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myapp/detail.html', {'question': question})
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
+
